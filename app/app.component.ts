e47aa5b283f84c55fe8b3503e380cf54b0432ef7
dubie <dubie@dubies-Mac.local>
2016-12-29 17:01:53 -0800

    basic nav bar added

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<router-outlet></router-outlet>'
+    template: `
+        <navbar></navbar>
+        <router-outlet></router-outlet>`
 })
 
 export class AppComponent{ }
--- app/app.module.ts
+++ app/app.module.ts
@@ -4,8 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BugModule } from './bugs/bug.module';
 import { AppRoutingModule } from './app-routing.module';
 
+// Component
 import { AppComponent } from './app.component';
-
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
@@ -13,7 +14,10 @@ import { AppComponent } from './app.component';
             BugModule,
             AppRoutingModule
             ],
-    declarations: [ AppComponent ],
+    declarations: [ 
+        AppComponent,
+        NavbarComponent
+         ],
     bootstrap: [ AppComponent ]
 })
 
