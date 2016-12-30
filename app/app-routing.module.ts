82f1c157560ede47c0abc646748644efe5cf5254
dubie <dubie@dubies-Mac.local>
2016-12-29 16:29:53 -0800

    Setup default routing to bugs module

---
+++ app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+
+@NgModule({
+    imports: [ RouterModule.forRoot([
+        { path: '', redirectTo: 'bugs', pathMatch: 'full'},
+        { path: '**', redirectTo: 'bugs'}
+    ])
+    ],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule{
+
+}
